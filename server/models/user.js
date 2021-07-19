d2866d7c513de5483122d9656335dafcbb5ef82d
joakin <akinbodejoshua145@gmail.com>
2021-07-19 06:33:33 +0100

    GET /users/me route and authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -6,7 +6,6 @@ const _ = require('lodash');
 var UserSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
         minlength: 1,
         require: true,
         unique: true,
@@ -32,6 +31,7 @@ var UserSchema = new mongoose.Schema({
     }]
 });
 
+
 UserSchema.methods.toJSON = function() {
     var user = this;
     var userObject = user.toObject();
@@ -48,6 +48,26 @@ UserSchema.methods.generateAuthToken = function() {
         return token;
     });
 }
+
+
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch(e) {
+        // return new Promise((resolve, reject) => {
+        //     reject();
+        // });
+        return Promise.reject();
+    };
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+} 
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {ObjectId} = require('mongodb');
+var {authenticate} = require('./middleware/authenticate');
 
 
 
@@ -47,6 +48,10 @@ app.post('/users', (req, res) => {
         res.send(e);
     });
 });
+
+app.get('/users/me', authenticate,  (req, res) => {
+    res.send(req.user);
+})
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
