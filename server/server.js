a80c9620d589af3bff5374133e751a3f2c47318b
joakin <akinbodejoshua145@gmail.com>
2021-08-23 05:06:12 +0100

    Added different routes and testing for login, deleting with authentication

--- server/server.js
+++ server/server.js
@@ -53,6 +53,13 @@ app.get('/users/me', authenticate,  (req, res) => {
     res.send(req.user);
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+});
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
@@ -62,9 +69,8 @@ app.post('/users/login', (req, res) => {
         })
     }).catch((e) => {
         res.status(400).send();
-    })
-   
-})
+    });
+});
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
